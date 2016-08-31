d8a5f7390070c54f7b80e3b9031b99047da42d9d
Akshit Goyal <goyalsharp@gmail.com>
2016-08-31 14:42:37 +0530

    added 3D

--- iris_pca.py
+++ iris_pca.py
@@ -1,13 +1,16 @@
 from sklearn.datasets import load_iris
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 f = load_iris()
-pca = PCA(n_components=2)
+pca = PCA(n_components=3)
 tfit = pca.fit_transform(f['data'])
 
-fig, ax = plt.subplots(1)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 col = ['r', 'limegreen', 'b']
 colors = [col[f['target'][i]] for i in range(len(f['data']))]
-plt.scatter(tfit[:, 0], tfit[:, 1], c=colors, s=60)
+
+plt.scatter(tfit[:, 0], tfit[:, 1], tfit[:, 2], c=colors, s=60, marker='o')
 plt.show()
